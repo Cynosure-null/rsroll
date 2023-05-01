18986b875c685c41d7bae91965cb7e39140bf82b
Cynosure-Null <cynosure@disroot.org>
2023-04-30 20:25:00 -0600

    undid the dum

--- src/main.rs
+++ src/main.rs
@@ -2,139 +2,54 @@ use cursive::{views::{EditView, self}, view, CursiveRunnable};
 #[allow(unused_imports)]
 
 use cursive::{event::Key, menu, traits::*, views::Dialog, views::TextArea};
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::{sync::atomic::{AtomicUsize, Ordering}, fmt::format};
 use cursive::Cursive;
+use std::thread;
 mod calc;
+mod music;
 
 fn main() {
     let mut siv = cursive::default();
 
+    let dice_arr = [100, 20, 12, 10, 8, 6, 4];
+
+    let songs = music::index();
+
     siv.menubar()
        .add_subtree(
            "Dice",
-           menu::Tree::new()
-               .leaf("d100", |s| {
-                   s.add_layer(
+           menu::Tree::new().with(move |tree|{
+               for i in 0..dice_arr.len(){
+                   let name = format!("d{}",dice_arr[i]);
+                   tree.add_leaf(name, move |s|{
+                       s.add_layer(
                        Dialog::new()
-                           .title("How many would you like to roll?")
-                           .content(
-                               EditView::new()
-                                   .on_submit(move |s, text| {
-                                       match text.parse::<i32>() {
-                                           Ok(n) => {
-                                               s.add_layer(Dialog::info(calc::roll(n, 100).to_string()));
-                                           },
-                                           Err(e) => {s.add_layer(Dialog::info(
-                                               "Error: input not an integer"
-                                           ));},
-                                       }
-                                   })
-                           ))})
+                               .title("How many would you like to roll?")
+                               .content(EditView::new()
+                                        .on_submit(move |s, text| {
+                                            match text.parse::<i32>() {
+                                                Ok(_n) => {
+                                                    s.add_layer(
+                                                        Dialog::info(
+                                                            calc::roll(
+                                                                _n, dice_arr[i]
+                                                            ).to_string()))
+                                                },
+                                                Err(_e) => {
+                                                    s.add_layer(
+                                                        Dialog::info(
+                                                            format!(
+                                                                "Error: {}",
+                                                                _e)
+                                                        ))}
 
-               .leaf("d20", |s| {
-                   s.add_layer(
-                       Dialog::new()
-                           .title("How many would you like to roll?")
-                           .content(
-                               EditView::new()
-                                   .on_submit(move |s, text| {
-                                       match text.parse::<i32>() {
-                                           Ok(n) => {
-                                               s.add_layer(Dialog::info(calc::roll(n, 20).to_string()));
-                                           },
-                                           Err(e) => {s.add_layer(Dialog::info(
-                                               "Error: input not an integer"
-                                           ));},
-                                       }
-                                   })
-                           ))})
-               .leaf("d12", |s| {
-                   s.add_layer(
-                       Dialog::new()
-                           .title("How many would you like to roll?")
-                           .content(
-                               EditView::new()
-                                   .on_submit(move |s, text| {
-                                       match text.parse::<i32>() {
-                                           Ok(n) => {
-                                               s.add_layer(Dialog::info(calc::roll(n, 12).to_string()));
-                                           },
-                                           Err(e) => {s.add_layer(Dialog::info(
-                                               "Error: input not an integer"
-                                           ));},
-                                       }
-                                   })
-                           ))})
-
-               .leaf("d10", |s| {
-                   s.add_layer(
-                       Dialog::new()
-                           .title("How many would you like to roll?")
-                           .content(
-                               EditView::new()
-                                   .on_submit(move |s, text| {
-                                       match text.parse::<i32>() {
-                                           Ok(n) => {
-                                               s.add_layer(Dialog::info(calc::roll(n, 10).to_string()));
-                                           },
-                                           Err(e) => {s.add_layer(Dialog::info(
-                                               "Error: input not an integer"
-                                           ));},
-                                       }
-                                   })
-                           ))})
-               .leaf("d8", |s| {
-                   s.add_layer(
-                       Dialog::new()
-                           .title("How many would you like to roll?")
-                           .content(
-                               EditView::new()
-                                   .on_submit(move |s, text| {
-                                       match text.parse::<i32>() {
-                                           Ok(n) => {
-                                               s.add_layer(Dialog::info(calc::roll(n, 8).to_string()));
-                                           },
-                                           Err(e) => {s.add_layer(Dialog::info(
-                                               "Error: input not an integer"
-                                           ));},
-                                       }
-                                   })
-                           ))})
-               .leaf("d6", |s| {
-                   s.add_layer(
-                        Dialog::new()
-                            .title("How many would you like to roll?")
-                            .content(
-                                EditView::new()
-                                    .on_submit(move |s, text| {
-                                        match text.parse::<i32>() {
-                                            Ok(n) => {
-                                                s.add_layer(Dialog::info(calc::roll(n, 6).to_string()));
-                                            },
-                                            Err(e) => {s.add_layer(Dialog::info(
-                                                "Error: input not an integer"
-                                            ));},
-                                        }
-                                    })
-                            ))})
-
-                .leaf("d4", |s| {
-                    s.add_layer(
-                        Dialog::new()
-                            .title("How many would you like to roll?")
-                            .content(
-                                EditView::new()
-                                    .on_submit(move |s, text| {
-                                        match text.parse::<i32>() {
-                                            Ok(n) => {
-                                                s.add_layer(Dialog::info(calc::roll(n, 4).to_string()));
-                                            },
-                                            Err(e) => {s.add_layer(Dialog::info(
-                                                "Error: input not an integer"
-                                            ));},
-                                        }
-                                    })
-                            ))}))
+                                            }
+                                        })
+                               )
+                       )
+                   })
+               }
+           }))
 
        .add_subtree(
            "NPC",
@@ -146,22 +61,39 @@ fn main() {
        .add_subtree(
            "Music",
            menu::Tree::new()
+               .with(move |tree| {
+                   for i in songs{
+                       let i_string = i.as_ref().unwrap().path().to_str().unwrap().to_string();
+                       tree.add_leaf(i_string.clone(), move |s| {
+                           let moveable_i = i_string.clone();
+                           thread::spawn(move || {
+                               music::play(moveable_i.clone());
+                           });
+                           s.add_layer(Dialog::info(format!("Now playing: {}", i_string.clone())));
+                       })
+                   }
+               })
                .leaf("song1", |s| {
-                   s.add_layer(Dialog::info("Now play a song"))
+                   thread::spawn(move || {
+                       music::play("test.ogg".to_string());
+                   });
+                   s.add_layer(Dialog::info("Now playing song1")
+                   )
                }))
 
-       .add_delimiter()
-       .add_leaf("quit", |s| s.quit());
+               .add_delimiter()
+               .add_leaf("quit", |s| s.quit());
 
-    // When `autohide` is on (default), the menu only appears when active.
-    // Turning it off will leave the menu always visible.
-    // Try uncommenting this line!
 
-    // siv.set_autohide_menu(false);
+           siv.set_autohide_menu(false);
 
-    siv.add_global_callback(Key::Esc, |s| s.select_menubar());
+    siv.select_menubar();
 
-    siv.add_layer(Dialog::text("Hit <Esc> to show the menu!"));
+    siv.add_global_callback(Key::Esc, |s| {
+        s.select_menubar()
+    });
+
+    eprintln!("Here");
 
     siv.run();
 }
